8c068037d2176d28eba07142663d23f8fcefc6c2
Jaiana Dixon <jdixon@wlapcs.org>
2018-01-23 19:13:46 +0000

    second commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+var rickyaSlider, jaiana, slider;
+
 function setup(){
     createCanvas(600,400);
+    background(0);
+    
+    //createSlider(min,max,defult)
+    rickyaSlider= createSlider(0,255,255);
+    //.position(x,y)
+    rickyaSlider.position(100,200);
+    
+    jaiana= createSlider(0,255,0);
+    jaiana.position(200,100);
+    
+    slider=createSlider(0,255,0);
+    slider.position(300,200);
 }
 
 function draw(){
+    redValue=rickyaSlider.value();
+    greenValue=jaiana.value();
+    blueValue=slider.value();
     
+    background(redValue,greenValue,blueValue);
 }
